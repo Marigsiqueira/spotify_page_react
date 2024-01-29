b39d9fb5d284e75a013bbfb7ebd98ef0bc6c5a3f
marigsiqueira <mgsmariana123@gmail.com>
2024-01-29 14:52:21 -0300

    Projeto com react

---
+++ src/Header/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import './Header.css';
+import smallRight from '../assets/icons/small-right.png';
+import smallLeft from '../assets/icons/small-left.png';
+import search from '../assets/icons/search.png'
+
+const Header = () => {
+    return (
+                <nav className="header__navigation">
+                <div className="navigation">
+                    <button className="arrow-left">
+                        <img src={smallLeft} alt=""/>
+                    </button>
+                    <button className="arrow-right">
+                        <img src={smallRight} alt=""/>
+                    </button>
+                </div>
+                    <div className="header__search">
+                        <img src={search} alt=""/>
+                        <input id="search-input" type="text" maxlength="800" placeholder="O que você quer ouvir?"/>
+                    
+                </div>
+
+                    <div class="header__login">
+                        <button className="subscribe">Inscreva-se</button>
+                        <button className="login">Entrar</button>
+                    </div>
+            </nav>
+    )
+};
+
+export default Header;
